b0e12ba8937cf8b0c2591c45f2eea768aa9bbfa4
Hornsby, Matt <Matt.L.Hornsby@nordstrom.com>
2016-07-19 21:42:25 -0700

    Adding calculation for days in each month

--- lib/satellite/twoline_to_satrec.ex
+++ lib/satellite/twoline_to_satrec.ex
@@ -44,8 +44,8 @@ defmodule Twoline_To_Satrec do
 
   end
 
-def epoch_year(year) when year < 57, do: 2000 + year
-def epoch_year(year), do: 1990 + year
+def epoch_year(year) when year < 57,  do: 2000 + year
+def epoch_year(year),                 do: 1990 + year
 
 def days2mdhms(year, days) do
   dayofyr = Float.floor(days) |> trunc |> IO.inspect
@@ -54,21 +54,27 @@ def days2mdhms(year, days) do
 end
 
 def day_and_month(year, dayofyr) do
-  lmonth = cond do
-    rem(year, 4) === 0 -> [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    true               -> [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  end
-  [head | tail] = lmonth
+  #lmonth = cond do
+  #  rem(year, 4) === 0 -> [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  #  true               -> [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  #end
 
+  lmonth = [31] ++ [days_in_februrary(year)] ++ [31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+  [head | tail] = lmonth
   _day_and_month(dayofyr, head, tail)
+
 end
+
+def days_in_februrary(year) when rem(year, 4) === 0 , do: 29
+def days_in_februrary(_year),                         do: 28
 
 defp _day_and_month(dayofyr, daysToAdd, [days_this_month | remaining_months])
     when (dayofyr > daysToAdd + days_this_month),
     do: _day_and_month(dayofyr, daysToAdd + days_this_month, remaining_months)
 
-defp _day_and_month(_dayofyr, daysToAdd, []), do: {daysToAdd, 12}
-defp _day_and_month(_dayofyr, daysToAdd, dayList), do: {daysToAdd, 12 - Enum.count(dayList) + 1}
+defp _day_and_month(_dayofyr, daysToAdd, []),       do: {daysToAdd, 12}
+defp _day_and_month(_dayofyr, daysToAdd, dayList),  do: {daysToAdd, 12 - Enum.count(dayList) + 1}
 
 
 
