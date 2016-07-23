0c481ac5bbdc71f99bc28fcf8438af0521b5b025
Hornsby, Matt <Matt.L.Hornsby@nordstrom.com>
2016-07-23 10:42:28 -0700

    Finally done with sgp4init (sans the deepspace stuff

--- lib/satellite/constants.ex
+++ lib/satellite/constants.ex
@@ -8,8 +8,10 @@ defmodule Constants do
   def x2o3, do: 2.0 / 3.0
   def j2, do: 0.00108262998905
   def j3, do: -0.00000253215306
+  def j4, do: -0.00000161098761
   def deg2rad, do: pi() / 180.0
   def two_pi, do: 2 * pi()
+  def j3oj2, do: j3 / j2
 
   def all do
     pi = pi()
@@ -19,18 +21,18 @@ defmodule Constants do
 
     %{
       :pi => pi,
-      :two_pi => 2 * pi,
-      :deg2rad => pi / 180.0,
+      :two_pi => two_pi,
+      :deg2rad => deg2rad,
       :rad2deg => 180 / pi,
       :minutes_per_day => 1440.0,
       :mu => mu,
       :earth_radius => earth_radius,
       :xke => xke,
-      :tumin => 1.0 / xke,
-      :j2 => j2(),
-      :j3 => j3(),
-      :j4 => -0.00000161098761,
-      :j3oj2 => j3() / j2(),
+      :tumin => tumin,
+      :j2 => j2,
+      :j3 => j3,
+      :j4 => j4,
+      :j3oj2 => j3 / j2,
       :x2o3 => x2o3()
     }
   end
--- lib/satellite/twoline_to_satrec.ex
+++ lib/satellite/twoline_to_satrec.ex
@@ -179,7 +179,6 @@ defmodule Twoline_To_Satrec do
             opsmode: satrec.operationmode
         }
 
-      # Stopped at line 217 in sgp4init.js
       initlResult = initl(initlParameters)
 
       satrec = %{satrec | no: initlResult.no}
@@ -199,6 +198,112 @@ defmodule Twoline_To_Satrec do
       satrec = %{satrec | gsto: initlResult.gsto}
       satrec = %{satrec | error: 0}
 
+      if (omeosq >= 0.0 || satrec.no >= 0.0) do
+        satrec = %{satrec | isimp: 0}
+        if (rp < 220.0 / Constants.earth_radius + 1.0) do
+          satrec = %{satrec | isimp: 1}
+        end
+        sfour = ss
+        qzms24 = qzms2t
+        perige = (rp - 1.0) * Constants.earth_radius
+
+        #  - for perigees below 156 km, s and qoms2t are altered -
+        if (perige < 156.0) do
+            sfour = perige - 78.0
+            if (perige < 98.0) do
+                sfour = 20.0
+            end
+            #  sgp4fix use multiply for speed instead of pow
+            qzms24temp =  (120.0 - sfour) / Constants.earth_radius
+            qzms24 = qzms24temp * qzms24temp * qzms24temp * qzms24temp
+            sfour = sfour / Constants.earth_radius + 1.0
+        end
+        pinvsq = 1.0 / posq
+
+        tsi = 1.0 / (ao - sfour)
+        satrec = %{satrec | eta: ao * satrec.ecco * tsi}
+        etasq = satrec.eta * satrec.eta
+        eeta = satrec.ecco * satrec.eta
+        psisq = abs(1.0 - etasq)
+        coef = qzms24 * :math.pow(tsi, 4.0)
+        coef1 = coef / :math.pow(psisq, 3.5)
+        cc2 = coef1 * satrec.no * (ao * (1.0 + 1.5 * etasq + eeta *
+                    (4.0 + etasq)) + 0.375 * Constants.j2 * tsi / psisq * satrec.con41 *
+                    (8.0 + 3.0 * etasq * (8.0 + etasq)))
+        satrec = %{satrec | cc1: satrec.bstar * cc2}
+        cc3 = 0.0
+        if (satrec.ecco > 1.0e-4) do
+          cc3 = -2.0 * coef * tsi * Constants.j3oj2 * satrec.no * sinio / satrec.ecco
+        end
+
+        satrec = %{satrec | x1mth2: 1.0 - cosio2}
+        satrec = %{satrec | cc4: 2.0 * satrec.no * coef1 * ao * omeosq *
+                            (satrec.eta * (2.0 + 0.5 * etasq) + satrec.ecco *
+                            (0.5 + 2.0 * etasq) - Constants.j2 * tsi / (ao * psisq) *
+                            (-3.0 * satrec.con41 * (1.0 - 2.0 * eeta + etasq *
+                            (1.5 - 0.5 * eeta)) + 0.75 * satrec.x1mth2 *
+                            (2.0 * etasq - eeta * (1.0 + etasq)) * :math.cos(2.0 * satrec.argpo)))}
+        satrec = %{satrec | cc5: 2.0 * coef1 * ao * omeosq * (1.0 + 2.75 *
+                            (etasq + eeta) + eeta * etasq)}
+        cosio4 = cosio2 * cosio2
+        temp1 = 1.5 * Constants.j2 * pinvsq * satrec.no
+        temp2 = 0.5 * temp1 * Constants.j2 * pinvsq
+        temp3 = -0.46875 * Constants.j4 * pinvsq * pinvsq * satrec.no
+        satrec = %{satrec | mdot: satrec.no + 0.5 * temp1 * rteosq * satrec.con41 + 0.0625 *
+                            temp2 * rteosq * (13.0 - 78.0 * cosio2 + 137.0 * cosio4)}
+        satrec = %{satrec | argpdot: (-0.5 * temp1 * con42 + 0.0625 * temp2 *
+                            (7.0 - 114.0 * cosio2 + 395.0 * cosio4) +
+                            temp3 * (3.0 - 36.0 * cosio2 + 49.0 * cosio4))}
+        xhdot1 = -temp1 * cosio
+        satrec = %{satrec | nodedot: xhdot1 + (0.5 * temp2 * (4.0 - 19.0 * cosio2) +
+                            2.0 * temp3 * (3.0 - 7.0 * cosio2)) * cosio}
+        xpidot =  satrec.argpdot+ satrec.nodedot
+        satrec = %{satrec | omgcof: satrec.bstar * cc3 * :math.cos(satrec.argpo)}
+        satrec = %{satrec | xmcof: 0.0}
+        if (satrec.ecco > 1.0e-4) do
+          satrec = %{satrec | xmcof: -x2o3 * coef * satrec.bstar / eeta}
+        end
+        satrec = %{satrec | nodecf: 3.5 * omeosq * xhdot1 * satrec.cc1}
+        satrec = %{satrec | t2cof: 1.5 * satrec.cc1}
+        if (abs(cosio+1.0) > 1.5e-12) do
+          satrec = %{satrec | xlcof: -0.25 * Constants.j3oj2 * sinio * (3.0 + 5.0 * cosio) / (1.0 + cosio)}
+        else
+          satrec = %{satrec | xlcof: -0.25 * Constants.j3oj2 * sinio * (3.0 + 5.0 * cosio) / temp4}
+        end
+
+        satrec = %{satrec | aycof: -0.5 * Constants.j3oj2 * sinio}
+        delmotemp = 1.0 + satrec.eta * :math.cos(satrec.mo)
+        satrec = %{satrec | delmo: delmotemp * delmotemp * delmotemp}
+        satrec = %{satrec | sinmao: :math.sin(satrec.mo)}
+        satrec = %{satrec | x7thm1: 7.0 * cosio2 - 1.0}
+
+        #  --------------- deep space initialization -------------
+        # TODO: Implement deep space initialization.
+        # I don't think this is needed for earth-orbiting satellites
+        # so I am blowing it off for now (it's hundreds of lines of code that I don't have a good way of testing at the moment)
+        # The code starts on line 331 in sgp4init.js
+
+        #----------- set variables if not deep space -----------
+        if (satrec.isimp !== 1) do
+            cc1sq = satrec.cc1 * satrec.cc1
+            satrec = %{satrec | d2: 4.0 * ao * tsi * cc1sq}
+            temp = satrec.d2 * tsi * satrec.cc1 / 3.0
+            satrec = %{satrec | d3: (17.0 * ao + sfour) * temp}
+            satrec = %{satrec | d4: 0.5 * temp * ao * tsi * (221.0 * ao + 31.0 * sfour) * satrec.cc1}
+            satrec = %{satrec | t3cof: satrec.d2 + 2.0 * cc1sq}
+            satrec = %{satrec | t4cof: 0.25 * (3.0 * satrec.d3 + satrec.cc1 * (12.0 * satrec.d2 + 10.0 * cc1sq))}
+            satrec = %{satrec | t5cof: 0.2 * (3.0 * satrec.d4 +
+                            12.0 * satrec.cc1 * satrec.d3 +
+                            6.0 * satrec.d2 * satrec.d2 +
+                            15.0 * cc1sq * (2.0 * satrec.d2 + cc1sq))}
+        end
+
+        sgp4(satrec, 0.0)
+        satrec = %{satrec | init: 'n'}
+        :ok
+
+      end
+
   end
 
   def initl(initlParameters) do
@@ -217,7 +322,7 @@ defmodule Twoline_To_Satrec do
     cosio2 = cosio * cosio
 
     #  ------------------ un-kozai the mean motion -----------------
-    ak = :math.pow(Constants.xke / no, Constants.x2o3);
+    ak = :math.pow(Constants.xke / no, Constants.x2o3)
     d1 = 0.75 * Constants.j2 * (3.0 * cosio2 - 1.0) / (rteosq * omeosq)
     delPrime = d1 / (ak * ak)
     adel = ak * (1.0 - delPrime * delPrime - delPrime *
@@ -227,12 +332,12 @@ defmodule Twoline_To_Satrec do
     ao = :math.pow(Constants.xke / no, Constants.x2o3)
     sinio = :math.sin(inclo)
     po = ao * omeosq
-    con42 = 1.0 - 5.0 * cosio2;
-    con41 = -con42 - cosio2 - cosio2;
-    ainv = 1.0 / ao;
-    posq = po * po;
-    rp = ao * (1.0 - ecco);
-    method = 'n';
+    con42 = 1.0 - 5.0 * cosio2
+    con41 = -con42 - cosio2 - cosio2
+    ainv = 1.0 / ao
+    posq = po * po
+    rp = ao * (1.0 - ecco)
+    method = 'n'
 
     #  sgp4fix modern approach to finding sidereal time
     if (opsmode === 'a') do
@@ -272,7 +377,7 @@ defmodule Twoline_To_Satrec do
             sinio: sinio,
             gsto: gsto
         }
-
+    initlResults
   end
 
   def gstime(jdut1) do
